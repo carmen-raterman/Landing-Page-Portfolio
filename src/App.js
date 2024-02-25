397985c1ccf470945157b2e12f2d580cd5265da3
carmen-raterman <ratersop@gmail.com>
2024-02-24 19:50:36 -0500

    updated Work page to function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Home from './Components/Home';
+import About from "./Components/About";
+import Work from "./Components/"
 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
+      <About />
+      <Work />
     </div>
   );
 }
